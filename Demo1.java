c7b11d4ecc65979c22b86915ae3a14e087d7d0cb
keerthireddy777 <vontikondakeerthireddy@gmail.com>
2026-01-31 15:49:54 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        double a=sc.nextDouble();
+        float b=sc.nextFloat();
+        String str2 =sc.nextLine();
+        System.out.println(age);
+        System.out.println(a);
+        System.err.println(b);
+        System.out.println(str2);
+
+        
+    }
+    
+}
