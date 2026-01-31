8d496e9328d4632d3366c2fdaac539ca2561f606
keerthireddy777 <vontikondakeerthireddy@gmail.com>
2026-01-31 15:28:00 +0530

    Add files via upload

---
+++ Untitled-1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class user{
+    public static void main(String[] args){
+        Scanner ob=new Scanner(System.in);
+        int age=ob.nextInt();
+        System.out.println(age);
+    }
+}
