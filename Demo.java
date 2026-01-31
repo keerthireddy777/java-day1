b33d0721b540c266ea5c1d2c27874c63706a1118
keerthireddy777 <vontikondakeerthireddy@gmail.com>
2026-01-31 14:36:10 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,7 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        Double r=434;
-        Double  area=3.14*r*r;
         System.out.println("area");
         System.out.print("Dharaniii");
     }
